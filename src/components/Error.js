334da564e4fbd476cd31f5f1bdc9913393d496f1
Tran Do Hoa <trandohoa93@gmail.com>
2023-01-31 09:47:50 +0700

    update

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from "react";
 const Error = () => {
-  return <h4>error element</h4>
-}
+  return (
+    <h4 className="section section-center text-center">
+      There Was An Error...
+    </h4>
+  );
+};
 
-export default Error
+export default Error;
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -7,11 +7,49 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
 const products_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  switch (action.type) {
+    case SIDEBAR_OPEN:
+      return { ...state, isSidebarOpen: true };
+    case SIDEBAR_CLOSE:
+      return { ...state, isSidebarOpen: false };
+    case GET_PRODUCTS_BEGIN:
+      return { ...state, products_loading: true };
+    case GET_PRODUCTS_SUCCESS:
+      const featured_products = action.payload.filter((product) => {
+        return product.featured === true;
+      });
+      return {
+        ...state,
+        products_loading: false,
+        products: action.payload,
+        featured_products,
+      };
+    case GET_PRODUCTS_ERROR:
+      return { ...state, products_loading: false, products_error: true };
+    case GET_SINGLE_PRODUCT_BEGIN:
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
+    case GET_SINGLE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product: action.payload,
+      };
+    case GET_SINGLE_PRODUCT_ERROR:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product_error: true,
+      };
+    default:
+      throw new Error();
+  }
+};
 
-export default products_reducer
+export default products_reducer;
